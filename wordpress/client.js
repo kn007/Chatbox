e369cdf02dd98cc749920a0c4f34b3863215dcde
Karl Chen <kn007@users.noreply.github.com>
2016-02-04 13:10:39 +0800

    add decodeURI

--- wordpress/client.js
+++ wordpress/client.js
@@ -105,7 +105,7 @@ $(function() {
 
         // For Wordpress to get username from cookie if exist
         if(getCookie(comment_author)!=='')
-            addCookie('chatname', getCookie(comment_author));
+            addCookie('chatname', decodeURI(getCookie(comment_author)));
 		
         // Read old username from cookie if exist
         if(getCookie('chatname')!=='')
